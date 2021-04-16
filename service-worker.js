16db59bb6847f82bd50df56a9c4abf6f77e03f58
Inna Sorochynska <innasoro@gmail.com>
2021-04-16 18:06:41 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.c3dc7c65e9718a0e34811e25c0bd7ce6.js"
 );
 
 self.addEventListener('message', (event) => {
